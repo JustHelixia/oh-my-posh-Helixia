66c6ec3201e785cfa3b76473d95af64d9fee7938
JustHelixia <129672190+JustHelixia@users.noreply.github.com>
2023-06-25 16:17:24 +0200

    Update oh-my-posh.lua

--- ConfigFiles/oh-my-posh.lua
+++ ConfigFiles/oh-my-posh.lua
@@ -1,4 +1,4 @@
 -- Install Clink first https://chrisant996.github.io/clink/
 -- Place this file in your script folder.
 -- Type "clink info" to din your script folder.
-load(io.popen('oh-my-posh init cmd --config C:/Users/Mandy/AppData/Local/Programs/oh-my-posh/themes/Mandycobaltcmd.omp.json'):read("*a"))()
+load(io.popen('oh-my-posh init cmd --config C:/Users/<WINDOWSUSERNAME>/AppData/Local/Programs/oh-my-posh/themes/Helixiacobaltcmd.omp.json'):read("*a"))()
