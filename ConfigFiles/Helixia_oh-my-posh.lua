96f361f80d5e5c701f974becd9727b195d42c184
JustHelixia <mandy_boskoop@live.nl>
2023-06-25 14:35:42 +0200

    Add the cmd configuration files.

---
+++ ConfigFiles/Helixia_oh-my-posh.lua
@@ -0,0 +1,4 @@
+-- Install Clink first https://chrisant996.github.io/clink/
+-- Place this file in your script folder.
+-- Type "clink info" to din your script folder.
+load(io.popen('oh-my-posh init cmd --config C:/Users/Mandy/AppData/Local/Programs/oh-my-posh/themes/Mandycobaltcmd.omp.json'):read("*a"))()
